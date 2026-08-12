22c241e32883b5b96567dd3320d177bad4233a3a
leonela957 <leonelatajaracruz@gmail.com>
2026-08-12 12:12:07 -0300

    12/08

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './paginas/inicio/inicio';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'inicio', redirectTo: 'inicio', pathMatch: 'full' }, 
+    { path: 'inicio', component: Inicio },
+    { path: '**', redirectTo: 'inicio' },
+
+];
